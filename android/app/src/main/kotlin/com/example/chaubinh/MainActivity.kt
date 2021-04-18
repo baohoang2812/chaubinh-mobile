175534d7604927f9d2b10eb503aaaca36bf747aa
Hoang Gia Bao <giabaohoang9199@gmail.com>
2021-04-19 03:54:41 +0700

    Init Project 2

---
+++ android/app/src/main/kotlin/com/example/chaubinh/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chaubinh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/blocs/readme.txt
@@ -0,0 +1,4 @@
+BLoC: Business Logic Component
+Purpose: 
+  - Separate business logic from UI
+  - Manage state of UI
